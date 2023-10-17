e5e54ae5d080f214cb18318b9e14a41465f60f63
Harsh_Khare <khareharsh026@gmail.com>
2023-10-17 13:19:49 +0530

    first commit

---
+++ src/images/UserImage.js
@@ -0,0 +1,6 @@
+     export default function UserImage(){
+      const   Image={  
+        data:12,
+    image:"https://i.pinimg.com/550x/76/75/b4/7675b498dcbe8ed610b63cccc16c8744.jpg",
+}
+     }
---
+++ src/reducers/user.js
@@ -0,0 +1,14 @@
+
+
+export default function user(state = {}) {
+ 
+   
+    return {
+      id: 1,
+      name: "Harsh Khare",
+      image: "https://i.pinimg.com/550x/76/75/b4/7675b498dcbe8ed610b63cccc16c8744.jpg",
+   
+  
+    }
+  
+  }
